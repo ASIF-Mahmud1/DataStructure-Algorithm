903073f2948c41db0a1d506ae8bb16d9f7967ec4
Asif <1421015@iub.edu.bd>
2022-02-09 14:04:06 +0600

    [buggy] reversing linked list

--- DailyProgress/Day-5/linked-list.js
+++ DailyProgress/Day-5/linked-list.js
@@ -11,7 +11,7 @@ class LinkedList {
         this.root = null
     }
 
-    addNode(node) {
+    insertLast(node) {
         if (this.root === null) 
         {
             this.root = node
@@ -35,15 +35,65 @@ class LinkedList {
 
     }
 
+    insertFirst(node){
+        node.next= this.root
+
+        this.root=node
+    }
+   
+    getFirst(){
+    
+        return this.root
+    }
+
+    getLast(){
+        let temp = this.root
+        while (temp.next != null) {
+            temp = temp.next
+        }
+      
+        return temp
+    }
+
+    reverseList(prev,current){
+      if( current)
+      {
+       
+        if(current.next===null)
+        {
+            console.log("Last");
+            current.next= prev
+            prev.next=null
+            console.log("Last", current ,prev);
+            return 
+        }
+        else 
+        {
+            console.log("goo deep",);
+            this.reverseList(current,current.next)
+        }
+      }
+      else 
+      {
+        console.log("YES");
+        this.reverseList(null,this.root)
+      }
+  
+    }
+
 }
 
 let nodes = [new Node(1), new Node(8), new Node(3), new Node(5),]
 let myList = new LinkedList()
 
-myList.addNode(nodes[0])
-myList.addNode(nodes[1])
-myList.addNode(nodes[2])
-myList.addNode(nodes[3])
-
+myList.insertLast(nodes[0])
+myList.insertLast(nodes[1])
+myList.insertLast(nodes[2])
+myList.insertLast(nodes[3])
+myList.insertFirst(new Node(89))
+myList.insertFirst(new Node(77))
+//myList.printList()
+myList.getLast()
+myList.getFirst()
+myList.reverseList()
 myList.printList()
-
