2394543fa27e2654e455c0360e0d28c9fbb7db28
Asif <1421015@iub.edu.bd>
2022-02-14 12:22:57 +0600

    check isomorphism using map data structure

---
+++ DailyProgress/Day-6/isomorphism-revisted.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} firstString
+ * @param {string} secondString
+ * @return {boolean}
+ */
+
+const checkIsomorphism=(firstString,secondString)=>{
+  const table= new Map()  
+  if(firstString.length===secondString.length)
+  {
+      for(let i=0; i< firstString.length;i++)
+      {
+          if(table.has(firstString[i])      )
+          {
+                if(table.get(firstString[i])=== secondString[i])
+                {
+                    // keep iteating
+                }
+                else 
+                {
+                    return false
+                }
+          }
+          else 
+          {
+                          
+             if([...table.values()].includes(secondString[i] ))
+             {
+                 return false
+             }
+             else 
+             {
+                table.set(firstString[i],secondString[i])
+             }
+          }
+      }
+      return true
+  }
+
+  else
+  {
+      return false
+  }
+
+  
+}
+
+const result= checkIsomorphism("efg", "bcc")
+console.log(result);
