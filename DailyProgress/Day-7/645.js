854058bb8f798bdb059f010d21794d4b5a2f8d68
Asif <1421015@iub.edu.bd>
2022-02-16 12:54:12 +0600

    [in progress] findErrornums

--- DailyProgress/Day-7/645.js
+++ DailyProgress/Day-7/645.js
@@ -3,21 +3,40 @@
  * @return {number[]}
  */
  var findErrorNums = function(nums) {
-    let table= new Set()
-    for(item of nums)
+    let table= new Map ()
+    for(let i=0;i <nums.length;i++)
     {
-        if (table.has(item))
+        if (table.has(nums[i]))
         {
-                 if(item===2 &&  nums.length===2)
-                {
-                    return [item, item-1]
-                }
-            return [item,item +1]
+              table.set(nums[i], {value:false, position:i})
+            
         }
         else 
         {
-            table.add(item)
+            table.set(nums[i], {value:false, position:i})
         }
     }
-    return []
+
+    console.log(table);
+    for(let i=1;i <=nums.length;i++)
+{
+    if(table.get(i)===undefined)
+    {
+        if(table.get(i-1)===true )
+        {
+         return[i,i+1]
+        }
+        else 
+        {
+            return[i-1,i]
+        }
+    }
+}
 };
+
+
+
+const nums= [2,2]
+
+const result= findErrorNums(nums)
+console.log(result);
