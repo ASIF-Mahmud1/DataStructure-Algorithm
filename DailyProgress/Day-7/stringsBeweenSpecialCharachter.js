ec3f190c183c9aef676db66e370bf11173f1c5d8
Asif <1421015@iub.edu.bd>
2022-02-16 14:30:32 +0600

    [done] sstring between special charachter

--- DailyProgress/Day-7/stringsBeweenSpecialCharachter.js
+++ DailyProgress/Day-7/stringsBeweenSpecialCharachter.js
@@ -3,96 +3,44 @@
  * @return {string}
  */
 
-const wordFreq = (string) => {
-    var words = string.replace(/[.]/g, '').split(/\s/);
-    var freqMap = {};
-    words.forEach(function (w) {
-        if (!freqMap[w]) {
-            freqMap[w] = 0;
-        }
-        freqMap[w] += 1;
-    });
+const table= ['p', 'sum', "divide"]
 
-    return freqMap;
-}
-
-function printSubsInDelimiters(str, startCode, endCode)
-{
-
-    // Stores the indices of
-    let dels = [];
-    for(let i = 0; i < str.length; i++)
-    {
-
-        // If opening delimiter
-        // is encountered
-        if (str[i] == startCode)
-        {
-            dels.push(i);
-        }
-
-        // If closing delimiter
-        // is encountered
-        else if ((str[i] == endCode) &&
-                 (dels.length > 0))
-        {
-
-            // Extract the position
-            // of opening delimiter
-            let pos = dels[dels.length - 1];
-
-            dels.pop();
-
-            // Length of substring
-            let len = i - 1 - pos;
-
-            // Extract the substring
-            let ans;
-            if(pos < len)
+ var stringsBetweenSpecialCharachter = function(list ) {
+    let result= ''
+   for(let i=0;i<list.length;i++)
+   {
+  
+       if(list[i]==="$")
+       {
+          let processed= list.slice(i+1, list.length)
+          let next$= processed.indexOf("$")
+          if(next$!==-1)
+          {
+            const query= processed.substring(0,next$).trim()
+            if(table[query]===undefined)
             {
-                ans = str.substring(pos + 1,
-                                   len + 1);
+                result= result+list[i]
             }
-            else{
-                ans = str.substring(pos + 1,
-                      len + pos + 1);
+            else 
+            {
+                result=result+ table[query]
+                 i= i+2
             }
-            console.log(ans);
-          //return ans
+            
+          }
 
-        }
-    }
-    console.log(dels);
-}
+       }
+       else 
+       {
+          result= result+list[i]
+       }
+   }
 
- var stringsBetweenSpecialCharachter = function(formula) {
-  let  startCode= "start", endCode= "end"
-    const startFreq= wordFreq(formula)[startCode]
-    const endFreq= wordFreq(formula)[endCode]
-    if(startFreq!==endFreq)
-    {
-        return formula
-    }
-    else 
-    {
-      let temp =formula
-   
-      let numberOfStartLeft= startFreq
-      while (numberOfStartLeft>0) {
-        let firstIndexOfStart = temp.indexOf('start')
-        let firstIndexOfEnd = temp.indexOf('end')
-        console.log(firstIndexOfStart,firstIndexOfEnd);
-        console.log(temp);
-        temp= temp.slice(firstIndexOfEnd+3)
-       numberOfStartLeft--
-      }
-     
-    }
-   
+   return result
     
 };
 
 
-const formula = "start 0 end + 5x+3+ start 9 end"
+const formula = "$0$ +$2$+ 5x+3+ $ 2 $"
 const result= stringsBetweenSpecialCharachter(formula)
 console.log(result)
