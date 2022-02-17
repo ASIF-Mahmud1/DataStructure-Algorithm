dcc803a05218eaaf2826d8a2c1d7783bf913d991
Asif <1421015@iub.edu.bd>
2022-02-17 14:11:08 +0600

    [in progress] need to work on when space is given on writing formula

--- DailyProgress/Day-7/stringsBeweenSpecialCharachter.js
+++ DailyProgress/Day-7/stringsBeweenSpecialCharachter.js
@@ -10,23 +10,28 @@ const table= ['p', 'sum', "divide", "divide", "divide", "divide", "divide", "div
    for(let i=0;i<list.length;i++)
    {
   
-       if(list[i]==="$")
+       if(list[i]==="~")
        {
           let processed= list.slice(i+1, list.length)
-          let next$= processed.indexOf("$")
+          let next$= processed.indexOf("~")
           if(next$!==-1)
           {
-            const query= processed.substring(0,next$).trim()
+            const query= processed.substring(0,next$).trim()         
             if(table[query]===undefined)
             {
                 result= result+list[i]
             }
             else 
             {
+             //  console.log("query ",query);
                 result=result+ table[query]
-                 i= i+ (processed.substring(0,next$).trim()).length
+                 i= i+ (processed.substring(0,next$+1).trim()).length
             }
             
+          }
+          else 
+          {
+            result= result+list[i]
           }
 
        }
@@ -41,6 +46,6 @@ const table= ['p', 'sum', "divide", "divide", "divide", "divide", "divide", "div
 };
 
 
-const formula = "$0$ +$5$+ 5x+3+ $26$"
+const formula = "~0~ +~5~+ 5x+3+ ~20~ 58+ ~0~"
 const result= stringsBetweenSpecialCharachter(formula)
 console.log(result)
